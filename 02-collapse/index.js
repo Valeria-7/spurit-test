77afa2f184d369aaadfbcbc257528503efba20be
Valeria Evseenkova <evseenkovavaleria2001@mail.ru>
2022-08-31 13:37:46 +0300

    implement collapse

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,28 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const collapsibleButton = document.querySelector('.collapsible__button');
+const collapsibleContent = document.querySelector('.collapsible__content');
+const collapsibleActionVisible = collapsibleButton.querySelector('.collapsible__action--visible');
+const collapsibleActionHidden = collapsibleButton.querySelector('.collapsible__action--hidden');
+collapsibleActionHidden.style.display = 'none';
+
+const contentHidding = collapsibleContent.animate(
+  { color: ["#000", "transparent"] },
+  { duration: 200, fill: "forwards" }
+)
+contentHidding.pause();
+
+function changeButton () {
+  collapsibleActionVisible.style.display = (collapsibleActionVisible.style.display == 'none') ?
+    'inline' : 'none';
+  collapsibleActionHidden.style.display = (collapsibleActionHidden.style.display == 'none') ?
+    'inline' : 'none';
+}
+
+function hideContent () {
+  if (contentHidding.playState === "paused") {
+    contentHidding.play();
+  } else {
+    contentHidding.reverse();
+  }
+  changeButton ();
+}
+collapsibleButton.addEventListener('click', hideContent);
